b5b0072c8ee5f6e85ee609738ff84dc49ba2db31
j1nxie <52886388+j1nxie@users.noreply.github.com>
2020-06-03 16:13:18 +0700

    Fixed folders, added symlink script

---
+++ symlink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln ./editor/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln ./editor/.vim/coc-settings.json ~/.vim/coc-settings.json
+ln ./git/.gitconfig ~/.gitconfig
+ln ./gui/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln ./gui/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln ./gui/.xprofile ~/xprofile
+ln ./config/peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
+ln ./config/peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+ln ./shell/.bashrc ~/.bashrc
+ln ./shell/.p10k.zsh ~/.p10k.zsh
+ln ./shell/.zshrc ~/.zshrc
