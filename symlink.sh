4b32428784e9c32076eacbd2e5db33f2ac1c2cf9
j1nxie <52886388+j1nxie@users.noreply.github.com>
2020-06-03 16:18:05 +0700

    Fixed symlink script

--- symlink.sh
+++ symlink.sh
@@ -6,8 +6,8 @@ ln ./git/.gitconfig ~/.gitconfig
 ln ./gui/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 ln ./gui/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
 ln ./gui/.xprofile ~/xprofile
-ln ./config/peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
-ln ./config/peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+ln ./peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
+ln ./peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 ln ./shell/.bashrc ~/.bashrc
 ln ./shell/.p10k.zsh ~/.p10k.zsh
 ln ./shell/.zshrc ~/.zshrc
