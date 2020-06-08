35aa6868086b4e584dfc04b25da2e91ebad692dc
j1nxie <52886388+j1nxie@users.noreply.github.com>
2020-06-08 20:44:26 +0700

    Update script (again)

--- symlink.sh
+++ symlink.sh
@@ -2,14 +2,14 @@
 
 mkdir ~/.vim
 mkdir -p ~/.config/{bspwm,nvim,sxhkd}
-ln -sf ./editor/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ./editor/.vim/coc-settings.json ~/.vim/coc-settings.json
-ln -sf ./git/.gitconfig ~/.gitconfig
-ln -sf ./gui/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
-ln -sf ./gui/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-ln -sf ./gui/.xprofile ~/xprofile
-ln -sf ./peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
-ln -sf ./peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-ln -sf ./shell/.bashrc ~/.bashrc
-ln -sf ./shell/.p10k.zsh ~/.p10k.zsh
-ln -sf ./shell/.zshrc ~/.zshrc
+ln -sf ~/configs/editor/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/configs/.vim/coc-settings.json ~/.vim/coc-settings.json
+ln -sf ~/configs/git/.gitconfig ~/.gitconfig
+ln -sf ~/configs/gui/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf ~/configs/gui/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -sf ~/configs/gui/.xprofile ~/.xprofile
+ln -sf ~/configs/peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
+ln -sf ~/configs/peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+ln -sf ~/configs/shell/.bashrc ~/.bashrc
+ln -sf ~/configs/shell/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/configs/shell/.zshrc ~/.zshrc
