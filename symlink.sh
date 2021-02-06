029485ab3211282931aaea80d764a2f379033916
j1nxie <rylieeeeexd@gmail.com>
2021-02-06 10:19:36 +0700

    Fixed symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -8,9 +8,6 @@ mkdir -p ~/.config/{bspwm,nvim,sxhkd,dunst,polybar,kitty}
 ln -sf ~/configs/editor/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/configs/.vim/coc-settings.json ~/.vim/coc-settings.json
 
-# Git config
-ln -sf ~/configs/git/.gitconfig ~/.gitconfig
-
 # Window manager (bspwm + sxhkd + dunst) configs
 ln -sf ~/configs/gui/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 ln -sf ~/configs/gui/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
@@ -20,8 +17,6 @@ ln -sf ~/configs/gui/.config/polybar/launch.sh ~/.config/polybar/launch.sh
 
 # X configs
 ln -sf ~/configs/gui/.xprofile ~/.xprofile
-ln -sf ~/configs/peripherals/etc/X11/xorg.conf.d/52-tablet.conf /etc/X11/xorg.conf.d/52-tablet.conf
-ln -sf ~/configs/peripherals/etc/X11/xorg.conf.d/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
 ln -sf ~/configs/gui/.xinitrc ~/.xinitrc
 
 # Shell configs
@@ -31,5 +26,5 @@ ln -sf ~/configs/shell/.zshrc ~/.zshrc
 ln -sf ~/configs/shell/.tmux.conf ~/.tmux.conf
 
 # Terminal configs
-ln -sf ~/configs/gui/.config/kitty/kitty.conf ~/.configs/kitty/kitty.conf
-ln -sf ~/configs/gui/.config/kitty/theme.conf ~/.configs/kitty/theme.conf
+ln -sf ~/configs/gui/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf ~/configs/gui/.config/kitty/theme.conf ~/.config/kitty/theme.conf
