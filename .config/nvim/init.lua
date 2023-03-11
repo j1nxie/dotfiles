56f3c1f26b686dbaa9bb53df1ed49ed7c6095135
j1nxie <rylieeeeexd@gmail.com>
2023-03-12 00:04:00 +0700

    feat: remove eslint_d diagnostics + add stylelint formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -131,10 +131,10 @@ require "dep" {
 					null_ls.builtins.code_actions.eslint_d,
 					null_ls.builtins.code_actions.gitrebase,
 					null_ls.builtins.code_actions.gitsigns,
-					null_ls.builtins.diagnostics.eslint_d,
 					null_ls.builtins.diagnostics.stylelint,
 					null_ls.builtins.diagnostics.ruff,
-					null_ls.builtins.formatting.prettierd,
+					null_ls.builtins.formatting.eslint_d,
+					null_ls.builtins.formatting.stylelint,
 					null_ls.builtins.formatting.ruff,
 				},
 				on_attach = function(client, bufnr)
