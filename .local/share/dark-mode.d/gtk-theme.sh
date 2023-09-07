a2109bbcfb451f657310b4bde1871c32f9c4bfa3
j1nxie <rylieeeeexd@gmail.com>
2023-09-07 17:30:45 +0700

    fix(darkman): properly set BAT_THEME

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -3,3 +3,4 @@
 gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Macchiato-Standard-Rosewater-dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 export BAT_THEME="Catppuccin-macchiato"
+fish --command="set -U BAT_THEME Catppuccin-macchiato"
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -3,3 +3,4 @@
 gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Rosewater-light
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 export BAT_THEME="Catppuccin-latte"
+fish --command="set -U BAT_THEME Catppuccin-latte"
