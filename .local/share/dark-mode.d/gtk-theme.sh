a79813861d1a0d627ce84c8cee596cd30e1cf55d
j1nxie <rylieeeeexd@gmail.com>
2023-09-07 16:03:45 +0700

    fix: more on-startup things

---
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Macchiato-Standard-Rosewater-dark
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+export BAT_THEME="Catppuccin-macchiato"
---
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Rosewater-light
+gsettings set org.gnome.desktop.interface color-scheme prefer-light
+export BAT_THEME="Catppuccin-latte"
