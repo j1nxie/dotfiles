82b91372b24bcb9aad59e958385383be650b3619
j1nxie <rylieeeeexd@gmail.com>
2023-09-09 10:41:13 +0700

    fix(fish): correct icat alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,7 +24,7 @@ end
 
 abbr -a lg "lazygit"
 abbr -a ly "lazygit -ucd ~/.local/share/yadm/lazygit -w ~ -g ~/.local/share/yadm/repo.git"
-abbr -a icat "kitten +icat"
+abbr -a icat "kitten icat"
 abbr -a nvide "neovide"
 abbr -a random "python3 ~/projects/scripts/random-org.py"
 abbr -a cat "bat --style=\"numbers,changes,header\""
