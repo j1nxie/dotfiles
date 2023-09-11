9c4603fc3e1c796e614f48335e7ce1dde3ee2b8e
j1nxie <rylieeeeexd@gmail.com>
2023-09-12 01:38:13 +0700

    fix(vscode-neovim): use find-it-faster

--- .config/nvim/vscode.lua
+++ .config/nvim/vscode.lua
@@ -23,4 +23,5 @@ vim.keymap.set("n", "k", "gk")
 vim.keymap.set("", "H", "^")
 vim.keymap.set("", "L", "$")
 
-vim.keymap.set("n", "<Leader>fg", "<Cmd>call VSCodeCall('workbench.action.findInFiles')<CR>")
+vim.keymap.set("", "<Leader>fg", "<Cmd>call VSCodeCall('find-it-faster.findWithinFiles')<CR>")
+vim.keymap.set("", "<Leader>ff", "<Cmd>call VSCodeCall('find-it-faster.findFiles')<CR>")
