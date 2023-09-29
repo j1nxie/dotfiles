b8dad3073faaa7fad73cb738f36214caf40345f1
j1nxie <rylieeeeexd@gmail.com>
2023-09-29 09:57:09 +0700

    feat(darkman): fix gtk theme name + set qt theme

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Macchiato-Standard-Rosewater-dark
+gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Macchiato-Standard-Rosewater-Dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 export BAT_THEME="Catppuccin-macchiato"
 fish --command="set -U BAT_THEME Catppuccin-macchiato"
 kitty +kitten themes --reload-in=all Catppuccin-Macchiato
+kvantummanager --set Catppuccin-Macchiato-Rosewater
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Rosewater-light
+gsettings set org.gnome.desktop.interface gtk-theme Catppuccin-Latte-Standard-Rosewater-Light
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 export BAT_THEME="Catppuccin-latte"
 fish --command="set -U BAT_THEME Catppuccin-latte"
 kitty +kitten themes --reload-in=all Catppuccin-Latte
+kvantummanager --set Catppuccin-Latte-Rosewater
