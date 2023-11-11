4ca1fca2f00205a5c00b3d66f441be94ef951091
j1nxie <rylieeeeexd@gmail.com>
2023-11-11 11:27:57 +0700

    feat(darkman): swaync + waybar toggles

--- .local/share/dark-mode.d/gtk-theme.sh
+++ .local/share/dark-mode.d/gtk-theme.sh
@@ -6,3 +6,7 @@ export BAT_THEME="Catppuccin-macchiato"
 fish --command="set -U BAT_THEME Catppuccin-macchiato"
 kitty +kitten themes --reload-in=all Catppuccin-Macchiato
 kvantummanager --set Catppuccin-Macchiato-Rosewater
+ln -sf ~/.config/waybar/macchiato.css ~/.config/waybar/theme.css
+ln -sf ~/.config/swaync/macchiato.css ~/.config/swaync/theme.css
+killall -SIGUSR2 waybar
+swaync-client -rs
--- .local/share/light-mode.d/gtk-theme.sh
+++ .local/share/light-mode.d/gtk-theme.sh
@@ -6,3 +6,7 @@ export BAT_THEME="Catppuccin-latte"
 fish --command="set -U BAT_THEME Catppuccin-latte"
 kitty +kitten themes --reload-in=all Catppuccin-Latte
 kvantummanager --set Catppuccin-Latte-Rosewater
+ln -sf ~/.config/waybar/latte.css ~/.config/waybar/theme.css
+ln -sf ~/.config/swaync/latte.css ~/.config/swaync/style.css
+killall -SIGUSR2 waybar
+swaync-client -rs
