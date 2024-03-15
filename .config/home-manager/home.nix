47b5336a5c60598fc974833760ccf0b9d74f69a6
j1nxie <rylieeeeexd@gmail.com>
2024-03-15 12:20:35 +0700

    home-manager: split git into own module + enable kitty themes

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./fish.nix
+    ./git.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -18,63 +19,9 @@
     };
   };
 
-  programs.kitty.enable = true;
-  programs.git = {
+  programs.kitty = {
     enable = true;
-    lfs.enable = true;
-    userName = "j1nxie";
-    userEmail = "rylieeeeexd@gmail.com";
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      user = {
-        signingkey = "/home/lumi/.ssh/id_ed25519.pub";
-      };
-      core = {
-        editor = "nvim";
-        pager = "delta";
-        autocrlf = "input";
-        excludesfile = "/home/lumi/.gitignore_global";
-      };
-      pull = {
-        rebase = true;
-      };
-      color = {
-        ui = "auto";
-      };
-      credential = {
-        helper = "store";
-      };
-      gpg = {
-        format = "ssh";
-      };
-      commit = {
-        gpgsign = true;
-      };
-      push = {
-        autoSetupRemote = true;
-      };
-      interactive = {
-        diffFilter = "delta --color-only";
-      };
-      delta = {
-        navigate = true;
-        light = true;
-      };
-      merge = {
-        conflictstyle = "diff3";
-      };
-      diff = {
-        colorMoved = "default";
-      };
-      "filter \"lfs\"" = {
-        clean = "git-lfs clean -- %f";
-        smudge = "git-lfs smudge -- %f";
-        process = "git-lfs filter-process";
-        required = true;
-      };
-    };
+    extraConfig = "include current-theme.conf";
   };
 
   # The home.packages option allows you to install Nix packages into your
