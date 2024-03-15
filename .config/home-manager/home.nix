ed3c462604fe0b3207860fdcafd877e729946c16
j1nxie <rylieeeeexd@gmail.com>
2024-03-15 12:38:22 +0700

    kitty: manage through hm

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -21,6 +21,11 @@
 
   programs.kitty = {
     enable = true;
+    # FIXME: ligature breaks in fish?
+    font = {
+      name = "Fira Code";
+      size = 11;
+    };
     extraConfig = "include current-theme.conf";
   };
 
