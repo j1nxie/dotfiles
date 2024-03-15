f954848cbbd753982ccac2168af8712bdc8778da
j1nxie <rylieeeeexd@gmail.com>
2024-03-15 13:19:21 +0700

    hm: pnpm + jbtoolbox

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -34,9 +34,11 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.vscode
-    pkgs.neofetch
+  home.packages = with pkgs; [
+    vscode
+    neofetch
+    jetbrains-toolbox
+    nodejs.pkgs.pnpm
   ];
 
   # Let Home Manager install and manage itself.
