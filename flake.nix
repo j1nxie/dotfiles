96c91b41ffbf2fea577336cdb4f544d5da504736
j1nxie <rylieeeeexd@gmail.com>
2024-04-03 09:54:41 +0700

    nixos: nixpkgs-unstable overlay

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
@@ -13,13 +14,27 @@
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     nixos-hardware,
     home-manager,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    system = "x86_64-linux";
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
+  in {
     nixosConfigurations.kotonya = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       modules = [
+        ({
+          configs,
+          pkgs,
+          ...
+        }: {nixpkgs.overlays = [overlay-unstable];})
         ./.config/nixos/configuration.nix
         nixos-hardware.nixosModules.lenovo-legion-15ach6
         home-manager.nixosModules.home-manager
