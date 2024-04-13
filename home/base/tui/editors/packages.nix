ace30290fefa129a56cfa38c7a82eeea7bd33e2f
j1nxie <rylieeeeexd@gmail.com>
2024-04-13 09:24:21 +0700

    pkgs: dotnet dev stuff

--- home/base/tui/editors/packages.nix
+++ home/base/tui/editors/packages.nix
@@ -1,9 +1,19 @@
-{pkgs, ...}: {
+{pkgs, ...}: let dotnetPkgs = (
+  with pkgs.dotnetCorePackages; combinePackages [
+    sdk_7_0
+    sdk_8_0
+]
+); in {
   home.packages = with pkgs; [
     clang
-    dotnet-sdk_7
+    dotnetPkgs
     nodePackages.pnpm
     nodejs_20
+    omnisharp-roslyn
     rustup
   ];
+
+  home.sessionVariables = {
+    DOTNET_ROOT = "${dotnetPkgs}";
+  };
 }
