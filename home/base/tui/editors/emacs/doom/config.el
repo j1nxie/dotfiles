702e94ea19657344e9a30260b02267d298d02672
j1nxie <rylieeeeexd@gmail.com>
2024-05-04 18:31:54 +0700

    emacs: goodbye centaur tabs

--- home/base/tui/editors/emacs/doom/config.el
+++ home/base/tui/editors/emacs/doom/config.el
@@ -152,39 +152,6 @@
 (after! treemacs
   (setq treemacs-select-when-already-in-treemacs 'close))
 
-;; centaur-tabs
-(defun centaur-tabs-hide-tab (x)
-  "Do no to show buffer X in tabs."
-  (let ((name (format "%s" x)))
-    (or
-     ;; Current window is not dedicated window.
-     (window-dedicated-p (selected-window))
-
-     ;; Buffer name not match below blacklist.
-     (string-prefix-p "*epc" name)
-     (string-prefix-p "*helm" name)
-     (string-prefix-p "*Helm" name)
-     (string-prefix-p "*Compile-Log*" name)
-     (string-prefix-p "*lsp" name)
-     (string-prefix-p "*company" name)
-     (string-prefix-p "*Flycheck" name)
-     (string-prefix-p "*tramp" name)
-     (string-prefix-p " *Mini" name)
-     (string-prefix-p "*help" name)
-     (string-prefix-p "*straight" name)
-     (string-prefix-p " *temp" name)
-     (string-prefix-p "*Help" name)
-     (string-prefix-p "*mybuf" name)
-     (string-prefix-p "Treemacs*" name)
-
-     ;; Is not magit buffer.
-     (and (string-prefix-p "magit" name)
-          (not (file-name-extension name)))
-     )))
-
-(after! centaur-tabs
-  (setq centaur-tabs-set-bar 'over))
-
 ;; :lang cc to use clangd
 (setq lsp-clients-clangd-args '("-j=3"
                                 "--background-index"
