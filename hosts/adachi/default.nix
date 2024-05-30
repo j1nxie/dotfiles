3c56b7651a8f0e17af7efa4ce1b574e23428ce1a
j1nxie <rylieeeeexd@gmail.com>
2024-05-30 23:19:21 +0700

    fix(adachi): correct mounts + hardware config

--- hosts/adachi/default.nix
+++ hosts/adachi/default.nix
@@ -38,17 +38,5 @@ in {
     driSupport32Bit = true;
   };
 
-  fileSystems."/mnt/stuff" = {
-    device = "/dev/disk/by-uuid/01D9D8D7E461C040";
-    fsType = "ntfs";
-    options = ["uid=1000" "gid=1000" "rw" "user" "exec" "umask=000" "x-gvfs-show"];
-  };
-
-  fileSystems."/mnt/more-stuff" = {
-    device = "/dev/disk/by-uuid/18E26960E26942DC";
-    fsType = "ntfs";
-    options = ["uid=1000" "gid=1000" "rw" "user" "exec" "umask=000" "x-gvfs-show"];
-  };
-
   system.stateVersion = "23.11";
 }
