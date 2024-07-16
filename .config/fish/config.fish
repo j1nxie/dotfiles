300be8692a3bfce1054a2c567de93069e588eeae
j1nxie <rylieeeeexd@gmail.com>
2024-07-16 21:57:47 +0700

    fish: config + plugins

---
+++ .config/fish/config.fish
@@ -0,0 +1,36 @@
+set -x EDITOR nvim
+set -x TERMINAL kitty
+set -x DIRENV_LOG_FORMAT ""
+set -x ANDROID_HOME ~/Android/Sdk
+set --global tide_left_prompt_items context $tide_left_prompt_items
+set --global tide_context_always_display true
+set --global fish_greeting
+
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
+
+function fish_greeting
+    echo
+    echo -e (uname -ro | awk '{print " \\\\e[1mos: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1muptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (uname -n | awk '{print " \\\\e[1mhostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo
+end
+
+alias ls="exa"
+alias la="exa -lAh"
+alias ll="exa -lh"
+alias l="exa -lah"
+alias lsa="exa -lah"
+
+alias lg="lazygit"
+alias ly="lazygit -ucd ~/.local/share/yadm/lazygit -w ~ -g ~/.local/share/yadm/repo.git"
+
+alias cat='bat --style="numbers,changes,header" --theme=$(if [ (gsettings get org.gnome.desktop.interface color-scheme) = "\'default\'" ]; echo "catppuccinLatte"; else; echo "catppuccinMacchiato"; end;)'
+
+# pnpm
+set -gx PNPM_HOME "/home/lumi/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
